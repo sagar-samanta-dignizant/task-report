fbee4703479f89b4dcdfc0b5bc33dbe876972962
sagar-samanta <sagar.samanta@dignizant.com>
2025-05-18 19:22:18 +0530

    ggg

--- generate-version.cjs
+++ generate-version.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const customVersion = "0.0.1";
+const customVersion = "0.0.2";
 const version = customVersion || Date.now();
 
 const filePath = path.resolve(__dirname, 'public', 'version.json');
