ac6d4596f07dd7e43cbe8bb903c5ec26bf58595d
sagar-samanta <sagar.samanta@dignizant.com>
2025-05-18 18:42:26 +0530

    feat: Add generate-version script to create version.json and update postbuild script in package.json

---
+++ generate-version.cjs
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const version = Date.now();
+
+const filePath = path.resolve(__dirname, 'public', 'version.json');
+fs.writeFileSync(filePath, JSON.stringify({ version }));
+
+console.log('✅ version.json generated with version:', version);
