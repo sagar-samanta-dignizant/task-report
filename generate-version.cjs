e98183af1e5726c337e7c183dbc3c18a1a55d9b6
sagar.samanta <sagar.samanta@gmail.com>
2025-05-19 09:51:46 +0530

    fix: Update version to 0.0.3 in generate-version and package.json

--- generate-version.cjs
+++ generate-version.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const customVersion = "0.0.2";
+const customVersion = "0.0.3";
 const version = customVersion || Date.now();
 
 const filePath = path.resolve(__dirname, 'public', 'version.json');
